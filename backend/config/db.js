2318a023b33e6a748e03bf1330d78a814af388b2
DevJustice161 <webdevjustice@gmail.com>
2026-08-08 00:12:28 +0100

    env stuffs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "fitly_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   charset: "utf8mb4",
 });
 
